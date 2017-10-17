04856da531edc861db518f78de0d77539f306c82
Andrew Pendleton <andrew@mapbox.com>
2017-10-17 17:40:07 -0400

    run all incandescent program

--- incn/main.py
+++ incn/main.py
@@ -1,6 +1,12 @@
 from scenes import *
 
 def program():
-    bars(10)
+    while True:
+        bars(20)
+        threedeesin(20)
+        scan(20)
+        spotlight(20)
+        meander(20)
+        splotch(20)
 
 run(program)
