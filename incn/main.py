95cacd3d23724eb206d22b2b089fcf2aa73d284c
Andrew Pendleton <andrew@mapbox.com>
2017-10-18 10:19:59 -0400

    uncomment scenes

--- incn/main.py
+++ incn/main.py
@@ -2,11 +2,11 @@ from scenes import *
 
 def program():
     while True:
-#        bars(20)
-#        threedeesin(20)
+        bars(20)
+        threedeesin(20)
         scan(20)
-#        spotlight(20)
-#        meander(20)
-#        splotch(20)
+        spotlight(20)
+        meander(20)
+        splotch(20)
 
 run(program)
