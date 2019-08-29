68f9fee4b091b862b18107419ea8aa2c11239b57
Andrew Pendleton <andrew@mapbox.com>
2019-08-29 00:16:03 -0400

    Updates for September show: remapped a couple of channels, re-timed and re-ordered scenes, and added a troubleshooting mode.

--- fluo/program.c
+++ fluo/program.c
@@ -1,10 +1,17 @@
 void loop() {
-    bars(30);
-    snake(30);
-    bars(30);
+    bars(25);
     drops(50);
+
     bars(30);
     randomFade(30,2);
+
     bars(30);
     spokes(15, true);
+
+    bars(30);
+    snake(30);
+
+    setAllLow();
+    delay(30 * 1000);
+    delay(30 * 1000);
 }
